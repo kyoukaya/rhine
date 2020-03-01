a00ba6339fe13228a5f7bb33928e814e71f1b910
kyoukaya <kyoukaya@gmail.com>
2020-03-01 18:37:18 +0800

    cleanup gamestate tests

--- proxy/gamestate/gamestate_test.go
+++ proxy/gamestate/gamestate_test.go
@@ -1,16 +1,22 @@
 package gamestate
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"testing"
 
-	"github.com/kyoukaya/go-lookup"
 	"github.com/kyoukaya/rhine/proxy/gamestate/statestruct"
-	"github.com/svyotov/mergo"
-	"github.com/tidwall/gjson"
 )
+
+type logShim struct{ t *testing.T }
+
+func (l logShim) Flush()                              {}
+func (l logShim) Println(i ...interface{})            { l.t.Log(i...) }
+func (l logShim) Printf(s string, i ...interface{})   { l.t.Logf(s, i...) }
+func (l logShim) Verboseln(i ...interface{})          { l.t.Log(i...) }
+func (l logShim) Verbosef(s string, i ...interface{}) { l.t.Logf(s, i...) }
+func (l logShim) Warnln(i ...interface{})             { l.t.Error(i...) }
+func (l logShim) Warnf(s string, i ...interface{})    { l.t.Errorf(s, i...) }
 
 func check(t *testing.T, err error) {
 	if err != nil {
@@ -28,54 +34,36 @@ func openAndRead(t *testing.T, filename string) []byte {
 // changed when an event is emitted.
 func TestHookWithPayload(t *testing.T) {
 	mod, _ := New(logShim{t})
-	mod.stateMutex.Unlock()
-	b := openAndRead(t, "testdata/syncdata.json")
-	syncData, err := unmarshalSyncData(b)
-	mod.state = &syncData.User
-	check(t, err)
-	b = openAndRead(t, "testdata/buildingsync.json")
-	res := gjson.GetBytes(b, "playerDataDelta.modified")
-	newUser, err := unmarshalUserData([]byte(res.Raw))
-	check(t, err)
-	err = mergo.Merge(mod.state, newUser, mergo.WithOverride)
-	check(t, err)
-
+	mod.handle("S/account/syncData", openAndRead(t, "testdata/syncdata.json"), nil)
 	testChan := make(chan StateEvent, 1)
+	done := make(chan error)
 	go func() {
-		for {
-			evt := <-testChan
-			fmt.Println(evt.Payload.(map[string]statestruct.EmptyStruct))
+		evt := <-testChan
+		payload := evt.Payload.(map[string]statestruct.EmptyStruct)
+		if len(payload) != 12 {
+			done <- fmt.Errorf("Expected a map with 12 entries in the payload")
 		}
+		close(done)
 	}()
-	mod.Hook("building.rooms.ELEVATOR", "test", testChan, true)
-	err = mod.WalkAndNotify([]byte(res.Raw))
-	check(t, err)
+	mod.Hook("building.rooms.ELEVATOR", "test", testChan, false)
+	mod.handle("S/building/sync", openAndRead(t, "testdata/buildingsync.json"), nil)
+	check(t, <-done)
 }
 
-type logShim struct{ t *testing.T }
-
-func (l logShim) Flush()                              {}
-func (l logShim) Println(i ...interface{})            { l.t.Log(i...) }
-func (l logShim) Printf(s string, i ...interface{})   { l.t.Logf(s, i...) }
-func (l logShim) Verboseln(i ...interface{})          { l.t.Log(i...) }
-func (l logShim) Verbosef(s string, i ...interface{}) { l.t.Logf(s, i...) }
-func (l logShim) Warnln(i ...interface{})             { l.t.Error(i...) }
-func (l logShim) Warnf(s string, i ...interface{})    { l.t.Errorf(s, i...) }
-
+func TestGamestate(t *testing.T) {
+	mod, _ := New(logShim{t})
+	mod.handle("S/account/syncData", openAndRead(t, "testdata/syncdata.json"), nil)
+}
 func TestModificationHooks(t *testing.T) {
 	const testPath = "dexNav.enemy.stage.camp_02"
-	data := openAndRead(t, "testdata/buildingsync.json")
-	b := []byte(gjson.GetBytes(data, "playerDataDelta.modified").Raw)
 	testChan := make(chan StateEvent, 1)
 	mod, _ := New(logShim{t})
-	mod.stateMutex.Unlock()
 	hook := mod.Hook(testPath, "test", testChan, false)
-	mod.stateMutex.Lock()
-	err := mod.WalkAndNotify(b)
-	mod.stateMutex.Unlock()
-	if err != nil {
-		t.Fatal(err)
-	}
+	mod.handle("S/account/syncData", openAndRead(t, "testdata/syncdata.json"), nil)
+	b := openAndRead(t, "testdata/buildingsync.json")
+	mod.handle("S/building/sync", openAndRead(t, "testdata/buildingsync.json"), nil)
+	// Block until the handle is done
+	mod.StateSync()
 	var evt StateEvent
 	select {
 	case evt = <-testChan:
@@ -83,14 +71,12 @@ func TestModificationHooks(t *testing.T) {
 		t.Fatal()
 	}
 	if evt.Path != testPath {
-		t.Fatal()
+		t.Fatal("evt.Path != testPath")
 	}
-	hook.Unhook()
 	// Shouldn't be able to get path from testChan anymore after unhooking
-	err = mod.WalkAndNotify(b)
-	if err != nil {
-		t.Fatal(err)
-	}
+	hook.Unhook()
+	mod.handle("S/building/sync", b, nil)
+	mod.StateSync()
 	select {
 	case <-testChan:
 		t.Fatal()
@@ -101,30 +87,35 @@ func TestModificationHooks(t *testing.T) {
 // map[string]interface{} did not add new entries in the map. Workaround with
 // map[string]struct{} instead.
 func TestMapInterfaceBug(t *testing.T) {
-	b := openAndRead(t, "testdata/syncdata.json")
-	syncData, err := unmarshalSyncData(b)
-	user := syncData.User
-	check(t, err)
-	data := openAndRead(t, "testdata/buildingsync.json")
-	b = []byte(gjson.GetBytes(data, "playerDataDelta.modified").Raw)
-	newUser, err := unmarshalUserData(b)
-	check(t, err)
-	err = mergo.Merge(&user, newUser, mergo.WithOverride)
-	check(t, err)
-	if _, exists := user.Building.Rooms.Elevator["slot_9"]; !exists {
+	mod, _ := New(logShim{t})
+	mod.handle("S/account/syncData", openAndRead(t, "testdata/syncdata.json"), nil)
+	mod.handle("S/building/sync", openAndRead(t, "testdata/buildingsync.json"), nil)
+	mod.StateSync()
+	if _, exists := mod.state.Building.Rooms.Elevator["slot_9"]; !exists {
 		t.Fatal()
 	}
 }
 
 func TestStructAccess(t *testing.T) {
-	b := openAndRead(t, "testdata/syncdata.json")
-	syncData, err := unmarshalSyncData(b)
+	mod, _ := New(logShim{t})
+	mod.handle("S/account/syncData", openAndRead(t, "testdata/syncdata.json"), nil)
+	val, err := mod.Get("building.rooms")
 	check(t, err)
-	user := syncData.User
-	val, err := lookup.LookupString(user, "Building.Rooms", false)
-	check(t, err)
-	vali := val.Interface()
-	unm, err := json.Marshal(vali)
-	check(t, err)
-	fmt.Printf("%s\n", unm)
+	rooms := val.(*statestruct.Rooms)
+	// Don't really want to go through the trouble of testing deep equality, so
+	// this will do for now.
+	if rooms.Dormitory["slot_20"].Comfort != 3000 || len(rooms.Corridor) != 8 {
+		t.Fatal("Rooms output not as expected.")
+	}
+}
+
+// mod.handle has a latency of about 150-200ms, there's a lot of room for improvement
+// here but it's usually not noticeable since it's typically faster than the game client.
+func BenchmarkHandleSync(b *testing.B) {
+	mod, _ := New(nil)
+	data := openAndRead(nil, "testdata/syncdata.json")
+	for i := 0; i < b.N; i++ {
+		mod.handle("S/account/syncData", data, nil)
+		mod.stateMutex.Lock()
+	}
 }
